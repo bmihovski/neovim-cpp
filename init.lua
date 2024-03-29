f053f9c81100ba8080bdacefc0967a3547689f37
bmihovski <bmihovski@live.com>
2024-03-29 12:00:20 +0200

    add telescope git files mapping

--- init.lua
+++ init.lua
@@ -3,8 +3,6 @@ vim.opt.expandtab = true
 vim.opt.shiftwidth = 2
 vim.opt.softtabstop = 2
 vim.opt.scrolloff = 8
-vim.opt.nu = true
-vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.smartindent = true
@@ -15,3 +13,5 @@ keymap("n", "<leader>f", "<cmd> CMakeRun <CR>", { noremap = true, silent = true 
 keymap("n", "<leader>s", "<cmd> CMakeCloseRunner <CR>", { noremap = true, silent = true })
 -- copilot chat
 keymap("n", "<leader>a", "<cmd> CopilotChatToggle <CR>", { noremap = true, silent = true })
+-- telescope
+keymap("n", "<leader>g", "<cmd> Telescope git_files <CR>", { noremap = true, silent = true })
